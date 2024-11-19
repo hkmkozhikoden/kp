13d9d97be86a430beb837f0cb0d3f8543b16c873
hakeem <hakeem@codeap.com>
2024-11-19 23:03:00 +0530

    contact page creation

---
+++ src/components/contact-from.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import '../assets/style/css/contact-form.css'
+function ContactForm() {
+  return (
+    <section className='contact-form-sec'>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-7 mb-3 mb-md-0">
+                    <div className="contact-info">
+                        <div className="row">
+                            <div className="col-12">
+                                <h4 className='font-medium mb-3'>Have a Creative Challenge? Let’s Solve It Together!</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur necessitatibus reprehenderit impedit inventore vero doloremque architecto debitis facilis sunt quod?</p>
+                            </div>
+                            <div className="col-12">
+                                <h5>Corporate Address</h5>
+                                <a href=''>Lorem ipsum dolor sit amet,<br /> consectetur adipisicing <br /> elit. Suscipit consectetur eveniet inventore sunt <br /> cupiditate dicta <br /> esse ullam repudiandae voluptate! Voluptatibus.</a>
+                            </div>
+                            <div className="col-12">
+                                <h5>Mail Us</h5>
+                               <a href="mailto:nisamudheenkp91@gmail.com">nisamudheenkp91@gmail.com</a> <br />
+                               <a href="mailto:nisamudheenkp91@gmail.com">nisamudheenkp91@gmail.com</a>
+                            </div>
+                            <div className="col-12">
+                                <h5>Telephone</h5>
+                               <a href="tel:+91 86060 65001">+91 86060 65001</a> <br />
+                               <a href="tel:+91 86060 65001">+91 86060 65001</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-5 mb-3 mb-md-0">
+                 <div className="contact-form">
+                  <form action="">
+                    <div className="row text-start">
+                      <div className="col-12">
+                        <h5 className='font-bold'>Get In Touch</h5>
+                        <p className='fs-16 mb-2'>How can i help you ?</p>
+                      </div>
+                      <div className="col-12 mb-3">
+                          <input className='form-control mt-2' type="text" id='Name' name='Name' placeholder='Enter your name' />
+                      </div>
+                      <div className="col-12 mb-3">
+                          <input className='form-control mt-2' type="number" id='FirstName' name='FirstName' placeholder='Enter phone number' />
+                      </div>
+                      <div className="col-12 mb-3">
+                          <input className='form-control mt-2' type="gmail" id='gmail' name='gmail' placeholder='Enter your gmail' />
+                      </div>
+                      <div className="col-12 mb-3">
+                          <textarea className='form-control' placeholder='Enter  your note ...' name="" id=""></textarea>
+                      </div>
+                      <div className="col-12 mt-3">
+                        <button className='btn btn-submit'> Send</button>
+                      </div>
+                    </div>
+                  </form>
+                 </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default ContactForm
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
-import PageBannner from '../components/page-banner'
-import Banner1 from '../assets/images/banner/contact-banner.jpg'
+import React from 'react';
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
+import PageBannner from '../components/page-banner';
+import Banner1 from '../assets/images/banner/contact-banner.jpg';
+import ContactForm from '../components/contact-from';
 function Contact() {
   return (
-    <>
+    <div className='contact-main'>
       <Navbar/>
-      <PageBannner 
-      title="Contact" 
-      subtitle="Dedicated to delivering exceptional results." 
-      image={Banner1} 
-      />
+      <PageBannner className="" title="Contact" subtitle="Dedicated to delivering exceptional results." image={Banner1} />
+      <ContactForm/>
       <Footer/>
-    </>
+    </div>
   )
 }
 
